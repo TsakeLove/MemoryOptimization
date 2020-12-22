f27fa3c46716d363965489f8292c452ef5dbd3f2
Ihor <tsake.love@gmail.com>
2020-12-22 16:05:22 +0200

    code

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Memory_optimization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int[,,] Matrix = new int[500, 500, 500];
+            int res = 0;
+
+            for (int i = 0; i < 500; i++)
+            {
+                for (int j = 0; j < 500; j++)
+                {
+                    for (int k = 0; k < 500; k++)
+                    {
+                        Matrix[k, j, i] += 2;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    for (int k = 0; k < 100; k++)
+                    {
+                        Matrix[k, j, i]--;
+                    }
+                }
+            }
+            
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+    }
+}
